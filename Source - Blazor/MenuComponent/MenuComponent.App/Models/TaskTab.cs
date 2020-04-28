faa864971cebe92b560e238c19d7047a45366d05
GeorgiPetrovGH <vortep.g@gmail.com>
2020-04-28 18:38:19 +0300

    Create Blazor project

---
+++ Source - Blazor/MenuComponent/MenuComponent.App/Models/TaskTab.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MenuComponent.App
+{
+    public class TaskTab
+    {
+        public string DisplayName { get; set; }
+
+        public ICollection<TaskTab> NestedTabs { get; set; }
+    }
+}
